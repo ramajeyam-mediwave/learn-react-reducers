f6fe196fd7978e501042221317cd75233a065fbb
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-10-16 16:02:29 +0530

    add isDone for a todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import { useReducer } from "react";
 import TodoList from "./components/TodoList";
 import TodoAddForm from "./components/TodoAddForm";
 
+/*
+{
+  id: 123,
+  text: 'Foo',
+  isDone: false
+}
+*/
 function App() {
   const [todos, dispatch] = useReducer(todoReducer, []);
 
@@ -14,12 +21,29 @@ function App() {
           {
             id: new Date().getTime(),
             text: action.value,
+            isDone: false,
           },
         ];
       }
       case "TODO_DELETE": {
         const filtered = todos.filter((t) => t.id != action.value);
         return [...filtered];
+      }
+      case "TODO_DONE": {
+        const newTodos = [...todos];
+        const idx = newTodos.findIndex((nt) => nt.id === action.value);
+        if (idx !== -1) {
+          newTodos[idx]["isDone"] = true;
+        }
+        return newTodos;
+      }
+      case "TODO_UNDONE": {
+        const newTodos = [...todos];
+        const idx = newTodos.findIndex((nt) => nt.id === action.value);
+        if (idx !== -1) {
+          newTodos[idx]["isDone"] = false;
+        }
+        return newTodos;
       }
       default: {
         throw Error("Unknown action: " + action.type);
@@ -39,13 +63,30 @@ function App() {
       value: id,
     });
   }
+  function handleDone(id, type) {
+    if (type == "done") {
+      dispatch({
+        type: "TODO_DONE",
+        value: id,
+      });
+    } else {
+      dispatch({
+        type: "TODO_UNDONE",
+        value: id,
+      });
+    }
+  }
 
   return (
     <>
       <h1>My todo</h1>
 
       <TodoAddForm handleAdd={handleAdd} />
-      <TodoList todos={todos} handleDelete={handleDelete} />
+      <TodoList
+        todos={todos}
+        handleDelete={handleDelete}
+        handleDone={handleDone}
+      />
     </>
   );
 }
